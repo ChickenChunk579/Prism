e4575dfec1420bdd5193417ce2877b377628672d
Rhys Tumelty <rhys@tumelty.co.uk>
2022-04-18 21:33:23 +0100

    Modified install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ echo PrismOS Installer
 echo NOTICE: This software is still in beta and is currently in development. Expect there to be bugs.
 echo Installing dependencies...
 # Install dependencies (lxde, python3, pip)
-sudo apt install lxde python3 python3-pip
+sudo apt install lxde python3 python3-pip xterm
 
 # Install Python Packages
-pip3 install pyglet numpy imgui[pyglet] xterm 
+pip3 install pyglet numpy imgui[pyglet] 
 echo Done!
 
 # Clone the repository
